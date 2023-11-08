c65f2a70e6b1a0a42d5357a4a92b9e66e41eac2d
AllRedCat <leonismogabriel@gmail.com>
2023-11-07 22:22:11 -0300

    Created template

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="/">Dashboard</a></li>
+            <li><a href="/deployments">Deployments</a></li>
+            <li><a href="/pipelines">Pipelines</a></li>
+            <li><a href="/monitoring">Monitoring</a></li>
+            <li><a href="/settings">Settings</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <main>
+        <section className="dashboard">
+          <h2>Dashboard</h2>
+          {/* Add dashboard content here */}
+        </section>
+
+        <section className="deployments">
+          <h2>Deployments</h2>
+          {/* Add deployments content here */}
+        </section>
+
+        <section className="pipelines">
+          <h2>Pipelines</h2>
+          {/* Add pipelines content here */}
+        </section>
+
+        <section className="monitoring">
+          <h2>Monitoring</h2>
+          {/* Add monitoring content here */}
+        </section>
+
+        <section className="settings">
+          <h2>Settings</h2>
+          {/* Add settings content here */}
+        </section>
+      </main>
     </div>
   );
 }
